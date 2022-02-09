a29651b1356693f809e57cec78d39cdd822344b7
Elena Chernikova <lena@acvos.com>
2022-02-09 13:03:39 -0800

    added test case for immutability of the original array

--- without.js
+++ without.js
@@ -1,34 +1,50 @@
-const without = function(sourceArr, itemsToRemoveArr) {
-  let newArr = [];
-  for (let i = 0; i < sourceArr.length; i++) {
-    if (sourceArr[i] !== itemsToRemoveArr[i]) {
-      newArr.push(sourceArr[i]);
+const without = function(source, itemsToRemove) {
+  const result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
     }
   } 
-  return newArr; 
+  return result; 
 };
 
-const eqArrays = function(newArr, sourceArr) {
-  if (newArr.length !== sourceArr.length) {
+const eqArrays = function(arr1, arr2) {  
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0, len = newArr.length; i < len; i++) {
-    if (newArr[i] !== sourceArr[i]) {
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = function(eqArrays) {
-  eqArrays === true ? console.log("Assertion passed") :
-    console.log("Assertion failed");
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed.`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed.`);
+  }
 };
 
-
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without(words, ['1', 'hello', [8]]));
 
+assertArraysEqual(
+  without(words, ['1', 'hello', [8]]), 
+  ["world", "lighthouse"]
+);
+assertArraysEqual(
+  without(words, ['lighthouse', 'hello']), 
+  ["world"]
+);
+assertArraysEqual(
+  without(words, ['1', '2', 3]), 
+  ["hello", "world", "lighthouse"]
+);
+
+/// Verify immutability of the oiginal array
+const original = ["hello", "world", "lighthouse"];
+const modifiedArray = without(original, ['lighthouse', 'hello']);
+assertArraysEqual(original, ["hello", "world", "lighthouse"]);
+  
