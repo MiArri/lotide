5d364af7bb6df1ca8f63c384b9619753561e8179
Elena Chernikova <lena@acvos.com>
2022-02-09 15:12:55 -0800

    function in progress

--- countOnly.js
+++ countOnly.js
@@ -11,7 +11,13 @@ const countOnly = function(allItems, itemsToCount) {
   
   for (const item of allItems) {
     console.log(item);
-    
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+      results[item] = 1;
+      }
+    }    
   }
   return results;
 }
