606fd65bd09bf7cb15bae92d549de22bf561b374
Elena Chernikova <lena@acvos.com>
2022-02-09 18:12:23 -0800

    letter position v1

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0, len = arr1.length; i < len; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed.`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed.`);
+//   }
+// };
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i]
+    if (!results[letter]) {
+      results[letter] = [i];      
+    } else {
+      results[letter].push(i);
+    } 
+  }
+  return results;
+};
+
+const letters = letterPositions("hello")
+assertArraysEqual(letters.e, [1]);
