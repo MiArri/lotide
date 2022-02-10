720c34a59626d5d192d183144b1bd9e35260ec1c
Elena Chernikova <lena@acvos.com>
2022-02-09 19:47:28 -0800

    lint done

--- eqObjects.js
+++ eqObjects.js
@@ -28,21 +28,21 @@ const eqObjects = function(object1, object2) {
       eqArrays(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
-    }    
+    }
   }
   return true;
 };
   
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); 
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false);
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); 
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); 
+assertEqual(eqObjects(cd, cd2), false);
 
