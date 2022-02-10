9adb7d75ec763e3174a30e5d7de3cac9e51ff4e8
Elena Chernikova <lena@acvos.com>
2022-02-09 17:05:57 -0800

    lint

--- raisinAlert.js
+++ raisinAlert.js
@@ -1,12 +1,12 @@
 const raisinAlarm = function(cookie) {
-  var chocolateCount = 0;
-  for (let i = 0; i < cookie.length; i++) { 
-    if (cookie[i] === "🍇"){
-      return "Raisin alert!"
-    } else if (cookie[i] === "🍫") { 
+  let chocolateCount = 0;
+  for (let i = 0; i < cookie.length; i++) {
+    if (cookie[i] === "🍇") {
+      return "Raisin alert!";
+    } else if (cookie[i] === "🍫") {
       chocolateCount += 1;
-    } 
-  };
+    }
+  }
   if (chocolateCount === cookie.length) {
     return "All good!";
   }
