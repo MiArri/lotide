6f44293a8733d9b7847c334a42935b767f8326b7
Elena Chernikova <lena@acvos.com>
2022-02-09 20:21:09 -0800

    inspect applied

---
+++ assertObjectsEqual.js
@@ -0,0 +1,45 @@
+const inspect = require('util').inspect
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const keysFirst = Object.keys(object1);
+  const keysSecond = Object.keys(object2);
+  if (keysFirst.length !== keysSecond.length) {
+    return false;
+  }
+  for (const key of keysFirst) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+  
+const assertObjectsEqual = function(actual, expected) {
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+
+
+
+
