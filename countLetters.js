f1505a1f9456a09159a0715864df71e37ddc4d69
Elena Chernikova <lena@acvos.com>
2022-02-09 16:15:09 -0800

    basic function

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const results = {};
+const countLetters = function(str){
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters('LHL')); 
