e0e243d9af3d73f0b693161e88f2eae4693fd94d
Elena Chernikova <lena@acvos.com>
2022-02-10 16:57:54 -0800

    map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed.`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed.`);
+  }
+};
+
+
+assertArraysEqual(words, results1);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(words, ["ground", "control", "to", "major"]);
