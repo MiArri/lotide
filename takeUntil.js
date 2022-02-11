08e5ade3800fac65079eb5c655cb70b9dce96bcc
Elena Chernikova <lena@acvos.com>
2022-02-10 17:51:00 -0800

    lint checked

--- takeUntil.js
+++ takeUntil.js
@@ -2,12 +2,12 @@ const takeUntil = function(array, callback) {
   let result = [];
   for (const item of array) {
     if (callback(item)) {
-      break
-    }  
+      break;
+    }
     result.push(item);
   }
   return result;
-}
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
