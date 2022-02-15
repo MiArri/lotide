921122733ccb6a21533ac25d735a6436e3f84868
Elena Chernikova <lena@acvos.com>
2022-02-14 16:15:39 -0800

    refactored with mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,18 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
-const tail = require("../tail");
+const assert = require('chai').assert;
+const head   = require('../tail');
 
 const words = ["Hello", "Lighthouse", "Labs"];
 
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(head(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(head(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("doesn't mutate the original  array", () => {
+    assert.equal(words.length, 3);
+  });
+});
